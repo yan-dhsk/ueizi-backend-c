79a8349f22392865bf86b00bd84936fbc6b2fbec
Yan Neves <yanvianaaa@gmail.com>
2025-11-03 12:54:35 -0300

    pequenas correções + novos modulos
    - Corrigido logicas erradas de ponteiros dentro da função "processarArquivo"
    - Corrigido conversão de trechoDaBr de int para float (type error)
    - Adição dos prototipos de função: salvarArquivo, carregarConsolidado, compararArquivos
    - Implementação de salvarArquivo
    - Parcial implementação de carregarConsolidado

--- load_files.c
+++ load_files.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 typedef struct Pedaco {
-int trechoDaBr;
+float trechoDaBr;
 char tipo;
 int upvotes;
 struct Pedaco *proximo;
@@ -17,17 +17,26 @@ struct Rua *proximo;
 } Estrada;
 
 void carregarArquivo(char nomeArquivo[]);
+void carregarConsolidado(char nomeConsolidado[]);
 void processarArquivo (Estrada **estradasPonteiro, Estrada *estradas, int br, float trechoDaBr, char tipo);
-void salvarArquivo(void);
+void compararArquivos (Estrada *estradas, Estrada *dados);
+void salvarArquivo(Estrada *estrada, char nomeConsolidado[]);
 
 void main(){
 char nomeArquivo[] = "alertas_100000_1.csv";
+char nomeConsolidade[] = "dadosConsolidados.csv";
 carregarArquivo (nomeArquivo);
+carregarConsolidado(nomeConsolidade);
 }
 
 void carregarArquivo (char nomeArquivo[]){
 FILE* carregado = fopen(nomeArquivo, "r");
-int lixo1, lixo2, br, trechoDaBr;
+if (carregado == NULL){
+    printf("Nao foi possivel abrir o arquivo");
+    return;
+}
+int lixo1, lixo2, br;
+float trechoDaBr;
 char tipo;
 int controle = 0;
 Estrada *estradas = NULL;
@@ -59,8 +68,8 @@ while (copia != NULL){
             if (trechoDaBr == copia2->trechoDaBr){
                 copia2->upvotes++;
                 break;
-            copia2 = copia2->proximo;
             }
+            copia2 = copia2->proximo;
         if (copia2 == NULL){
             Trecho *noTrecho = (Trecho*) malloc(sizeof(Trecho));
             copia2->proximo = noTrecho;
@@ -74,7 +83,7 @@ while (copia != NULL){
     copia = copia->proximo;
     copia2 = copia->inicioTrecho;
     }
-if (copia->proximo == NULL || *estradasPonteiro == NULL){controle++;}
+if (*estradasPonteiro == NULL){controle++;}
 else if (copia->proximo == NULL || *estradasPonteiro != NULL){controle = 2;}
 if (controle != 0) {
     Estrada *noBR = (Estrada*) malloc(sizeof(Estrada));
@@ -86,7 +95,7 @@ if (controle != 0) {
     noTrecho->trechoDaBr = trechoDaBr;
     noTrecho->upvotes = 1;
     if (controle == 1){
-    estradas = noBR;
+    *estradasPonteiro = noBR;
     } else {
     copia->proximo = noBR;
     }
@@ -94,4 +103,51 @@ if (controle != 0) {
 }
 }
 
+void carregarConsolidado(char nomeConsolidado[]){
+FILE *consolidado = fopen(nomeConsolidado, "r");
+if (consolidado == NULL) {
+    printf("Nao existe arquivos anteriormente salvo\n");
+    printf("Um novo arquivo sera criado\n");
+    return;
+}
+Estrada *dados = NULL;
+int controle = 0;
+int br, upvotes;
+float trecho;
+char tipo;
+Estrada *copia = dados;
+while (fscanf(consolidado, "%d;%d;%c;%f", &br, &trecho, &tipo, &upvotes) == 4){
+if (controle == 0){
+    controle++;
+    continue;
+}
+if (dados == NULL || copia->br != br){
+Estrada *noBR = (Estrada*) malloc(sizeof(Estrada));
+noBR->br = br;
+noBR->proximo = NULL;
+Trecho *noTrecho = (Trecho*) malloc(sizeof(Trecho));
+noBR->inicioTrecho = noTrecho;
+noTrecho->trechoDaBr = trecho;
+}
+copia = copia->proximo;
+}
+}
+
+void salvarArquivo(Estrada *estrada, char nomeConsolidado[]){
+FILE *salvo = fopen(nomeConsolidado, "w");
+if (salvo == NULL){
+    printf("Erro ao salvar arquivo");
+    return;
+}
+Estrada *copia = estrada;
+Trecho *copia2 = copia->inicioTrecho;
+fprintf(salvo, "br;trecho;tipo;upvotes");
+while (copia->inicioTrecho != NULL){
+    fprintf(salvo, "%d;%d;%c;%d\n", copia->br, copia2->trechoDaBr, copia2->tipo, copia2->upvotes);
+    copia2 = copia2->proximo;
+    copia = copia->proximo;
+}
+printf("Arquivo salvo com sucesso!");
+fclose(salvo);
+}
 
