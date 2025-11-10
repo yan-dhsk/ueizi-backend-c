ed783992ccc617c4bbc6b508173f772cc2bcc80d
Vinicius Meneses da Silva <viniciusmeneses639@gmail.com>
2025-11-10 19:35:52 -0300

    Update trecho_especifico.c

--- trecho_especifico.c
+++ trecho_especifico.c
@@ -8,37 +8,27 @@ void ler(char nomeArq[], int br, float trecho1, float trecho2){
 FILE *arquivo=fopen(nomeArq, "r");
 FILE *avisos=fopen("alertas_por_br.csv","w");
 
+if(arquivo==NULL){printf("Erro, arquivo de dados nao encontrado");return;}
+if(avisos==NULL){printf("Erro ao criar arquivos de alertas");return;}
+
 int campo1=0,campo4=0;
 float campo2=0.0;
 char campo3='x';
-
-if(arquivo==NULL){printf("Erro, arquivo de dados nao encontrado");return;}
-if(arquivo==NULL){printf("Erro, arquivo de alertas nao encontrado");return;}
 
 fscanf(arquivo, "%*[^\n]\n");
 fprintf(avisos, "br;km;tipoAlerta;upvotes \n");
 while(fscanf(arquivo, "%d;%f;%c;%d\n", &campo1, &campo2, &campo3, &campo4)==4){
         if(campo1==br && campo2>=trecho1 && campo2<=trecho2){
-            printf("%d;%.1f;%c;%d\n", campo1, campo2, campo3, campo4);
             fprintf(avisos, "%d;%.1f;%c;%d\n", campo1, campo2, campo3, campo4);
             }
 }
 fclose(avisos);
 fclose(arquivo);
 }
-
-void criararquivo(){
-
-FILE *arquivo=fopen("alertas_por_br.csv", "w");
-
-if (arquivo==NULL){printf("Erro ao criar arquivo");return;}
-}
-
 int main(){
 int br;
 float trecho1, trecho2;
 char nomearquivo[]="dadosConsolidados.csv";
 printf("Digite a BR e o trecho especifico da br (Ex: 102(BR) 150(Inicio do trecho) 170(final do trecho))\n");
 scanf("%d %f %f", &br, &trecho1, &trecho2);
-criararquivo();
 ler(nomearquivo, br, trecho1, trecho2);}
