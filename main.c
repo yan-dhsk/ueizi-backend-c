e0185f72308efc5da5367d349637660f4684df00
Yan Neves <yanvianaaa@gmail.com>
2025-11-11 17:08:35 -0300

    Implementação de recursividade
    - Adição da opção de finalizar programa
    - codigo nao finaliza até que o úsuario decida

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 #include "load_files.h"
 
 void main () {
+while(1){
 printf("===================Ola, bem-vindo ao sistema Uezi=====================\n");
-printf("1 - Carregar e processar novos arquivos\n2 - Consultar trechos especificos\n3 - Organizar arquivos por BR's\n");
+printf("1 - Carregar e processar novos arquivos\n2 - Consultar trechos especificos\n3 - Organizar arquivos por BR's\n0 - Finalizar o programa\n");
 printf("Porfavor, selecione sua opcao: ");
 int escolha;
 scanf("%d", &escolha);
 switch (escolha){
 case 1:
-    printf("Porfavor, digite o nome do arquivo a ser carregado: ");
+    printf("Porfavor, digite o nome (junto da extensao) do arquivo a ser carregado: ");
     char nomeArquivo[200];
     getchar();
     fgets(nomeArquivo, 200, stdin);
@@ -33,12 +34,17 @@ case 2:
     printf("Digite a BR e o trecho especifico da br (Ex: 102(BR) 150(Inicio do trecho) 170(final do trecho))\n");
     scanf("%d %f %f", &br, &trecho1, &trecho2);
     ler(nomearquivo, br, trecho1, trecho2);
+    printf("Arquivo salvo com sucesso!\n");
     break;
 case 3:
 char nomeArquivos[]="dadosConsolidados.csv";
 lerBrs(nomeArquivos);
+printf("Arquivo salvo com sucesso!\n");
 break;
+case 0:
+    printf("Encerrando programa...");
+    return;
 default:
     printf("Valor invladio!\n");
     break;
-}}
+}}}
